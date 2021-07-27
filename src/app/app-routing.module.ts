0ab78365f189ac8327f04c99c3f1deb4d9b7827b
sanket8081 <sanket8081@gmail.com>
2021-07-28 04:49:57 +0530

    Register routing fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"app-login",component:LoginComponent},
+  {path:"app-register",component:RegisterComponent},
+  {path:"",component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
