d1bf8262dec0719125174c8cd646e5a2a775c5db
sanket8081 <sanket8081@gmail.com>
2021-08-12 01:33:12 +0530

    register page changes

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterComponent implements OnInit {
 
   constructor() { }
-
+  passType: string = 'password';
   ngOnInit(): void {
   }
 
@@ -19,6 +19,28 @@ export class RegisterComponent implements OnInit {
     if (event.keyCode != 8 && !pattern.test(inputChar)) {
       event.preventDefault();
     }
+    
   }
 
+  keyPressPassCheck(event:any){
+    //const passwordPattern=/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
+    var inp = String.fromCharCode(event.keyCode);
+
+    if (/[a-zA-Z0-9-_ ]/.test(inp)) {
+      return true;
+    } else {
+      event.preventDefault();
+      return false;
+    }
+  }
+
+  changePasswordType(){
+    if(this.passType== 'password'){
+    this.passType= 'text'
+    }else{
+    this.passType== 'password'
+    }
+    }
+
+   
 }
